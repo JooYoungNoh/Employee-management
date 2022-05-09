21b82d1495158520f29b3ddacb8039fc9a564ecf
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-10 00:38:48 +0900

    fix: 내비게이션 삭제

--- EmployeeManagement/EmployeeManagement/FindIDVC.swift
+++ EmployeeManagement/EmployeeManagement/FindIDVC.swift
@@ -12,18 +12,11 @@ class FindIDVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        uiDeployment()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: UI 배치(No Storyboard)
+    func uiDeployment(){
+        
     }
-    */
-
 }
