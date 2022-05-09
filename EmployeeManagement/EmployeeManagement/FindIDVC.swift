4132e4d4faf863c07e8a39582228b003cb5a513d
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-10 00:27:03 +0900

    add: 아이디, 비밀번호 찾기 회원가입 화면 생성 및 연걸

---
+++ EmployeeManagement/EmployeeManagement/FindIDVC.swift
@@ -0,0 +1,29 @@
+//
+//  FindIDVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/05/10.
+//
+
+import UIKit
+
+class FindIDVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EmployeeManagement/EmployeeManagement/FindPasswordVC.swift
@@ -0,0 +1,29 @@
+//
+//  FindPasswordVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/05/10.
+//
+
+import UIKit
+
+class FindPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
