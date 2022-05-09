a1aad614244c1324491c43143db6e479781ab0b4
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-09 23:16:54 +0900

    add: 로고, 아이디, 비밀번호 UI 생성

--- EmployeeManagement/EmployeeManagement/LoginVC.swift
+++ EmployeeManagement/EmployeeManagement/LoginVC.swift
@@ -16,10 +16,41 @@ class LoginVC: UIViewController {
         let logo = UIImage(named: "management")
         let logoImage = UIImageView(image: logo)
         
-        logoImage.frame = CGRect(x: self.view.frame.width/2 - 70, y: 150, width: 140, height: 120)
+        logoImage.frame = CGRect(x: self.view.frame.width/2 - 70, y: 200, width: 140, height: 120)
         
         self.view.addSubview(logoImage)
         
+        //아이디
+        let id = UIImage(systemName: "person.circle")
+        let idImage = UIImageView(image: id)
+        let idTextField = UITextField()
+        
+        idImage.frame = CGRect(x: 80, y: 350, width: 30, height: 30)
+        idImage.tintColor = UIColor.systemGray4
+        
+        idTextField.frame = CGRect(x: 120, y: 350, width: 180, height: 30)
+        idTextField.placeholder = "id"
+        idTextField.borderStyle = .roundedRect
+        idTextField.font = UIFont.init(name: "Chalkboard SE", size: 14)
+        
+        self.view.addSubview(idImage)
+        self.view.addSubview(idTextField)
+        
+        //비밀번호
+        let pw = UIImage(systemName: "lock.circle")
+        let pwImage = UIImageView(image: pw)
+        let pwTextField = UITextField()
+        
+        pwImage.frame = CGRect(x: 80, y: 400, width: 30, height: 30)
+        pwImage.tintColor = UIColor.systemGray4
+        
+        pwTextField.frame = CGRect(x: 120, y: 400, width: 180, height: 30)
+        pwTextField.placeholder = "password"
+        pwTextField.borderStyle = .roundedRect
+        pwTextField.font = UIFont.init(name: "Chalkboard SE", size: 14)
+        
+        self.view.addSubview(pwImage)
+        self.view.addSubview(pwTextField)
 
     }
     
