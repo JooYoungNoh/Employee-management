6ef38d88e7e931db6fdef680d37b9e396041c2f1
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-09 22:10:56 +0900

    add: 로고 이미지

--- EmployeeManagement/EmployeeManagement/LoginVC.swift
+++ EmployeeManagement/EmployeeManagement/LoginVC.swift
@@ -11,19 +11,17 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //MARK: UI 배치(No Storyboard)
+        //로고 이미지
+        let logo = UIImage(named: "management")
+        let logoImage = UIImageView(image: logo)
+        
+        logoImage.frame = CGRect(x: self.view.frame.width/2 - 70, y: 150, width: 140, height: 120)
+        
+        self.view.addSubview(logoImage)
+        
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
