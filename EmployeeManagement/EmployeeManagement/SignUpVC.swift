3223a118a1cfe841d65cdc2d04a8db3de9991ff7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-10 19:07:11 +0900

    add: 닫기 버튼, 회원가입 레이블 생성

--- EmployeeManagement/EmployeeManagement/SignUpVC.swift
+++ EmployeeManagement/EmployeeManagement/SignUpVC.swift
@@ -12,18 +12,32 @@ class SignUpVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        signUpUIDeployment()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: UI 배치(Find ID)
+    func signUpUIDeployment(){
+        //닫기 버튼
+        let closeButton = UIButton()
+        
+        closeButton.frame = CGRect(x: 20, y: 50, width: 50, height: 40)
+        
+        closeButton.setTitle("Close", for: .normal)
+        closeButton.setTitleColor(UIColor.black, for: .normal)
+        closeButton.titleLabel?.font = UIFont.init(name: "Chalkboard SE", size: 20)
+        
+        self.view.addSubview(closeButton)
+        
+        //아이디 찾기 레이블
+        let signUp = UILabel()
+        
+        signUp.frame = CGRect(x: self.view.frame.width/2 - 80, y: 90, width: 160, height: 50)
+        signUp.text = "Sign Up"
+        signUp.textColor = UIColor.black
+        signUp.font = UIFont.init(name: "Chalkboard SE", size: 30)
+        signUp.textAlignment = .center
+        
+        self.view.addSubview(signUp)
     }
-    */
 
 }
