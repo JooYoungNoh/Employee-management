b4ae3ad988f9f7b3ebee014822c0ad45efffdd45
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-11 21:36:25 +0900

    add: 유저데이터 파일 생성

---
+++ EmployeeManagement/EmployeeManagement/UsersData.swift
@@ -0,0 +1,21 @@
+//
+//  UsersData.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/05/11.
+//
+
+import UIKit
+import FirebaseFirestore
+
+class UsersData{
+    var userID: String?
+    var userPW: String?
+    var pwQusetion: String?
+    var pwAnswer: String?
+    var username: String?
+    var userBirth: String?
+    var userPhone: String?
+    var userGender: Int?
+    var userJob: Int?
+}
