569a70b7de3e09289ca542d3474a9aafe88ae01c
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-17 00:32:38 +0900

    fix: 테이블 뷰 컨트롤러 삭제 후 뷰 컨트롤러 생성

--- EmployeeManagement/EmployeeManagement/ShopVC.swift
+++ EmployeeManagement/EmployeeManagement/ShopVC.swift
@@ -2,39 +2,27 @@
 //  ShopVC.swift
 //  EmployeeManagement
 //
-//  Created by 노주영 on 2022/05/15.
+//  Created by 노주영 on 2022/05/17.
 //
 
 import UIKit
 
-class ShopVC: UITableViewController {
-    
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+class ShopVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+    
 
     /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    // MARK: - Navigation
 
-        // Configure the cell...
-
-        return cell
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
     */
 
