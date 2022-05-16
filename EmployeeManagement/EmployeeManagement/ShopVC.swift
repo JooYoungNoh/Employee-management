624d47dee40f8d98c9e154fd261bb62a34c20162
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-17 01:17:54 +0900

    add: 매장 찾기 화면 UI 구성

--- EmployeeManagement/EmployeeManagement/ShopVC.swift
+++ EmployeeManagement/EmployeeManagement/ShopVC.swift
@@ -6,24 +6,49 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ShopVC: UIViewController {
+    
+    let db = Firestore.firestore()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
+    //화면 구성 객체
+    let titleLabel = UILabel()
+    let searchBar = UISearchBar()
+    let tableview = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        uiDeployment()
+    }
+    
+    func uiDeployment(){
+        //타이틀 UI
+        self.titleLabel.frame = CGRect(x: 20, y: 60, width: 80, height: 40)
+        self.titleLabel.text = "Shop"
+        self.titleLabel.textColor = UIColor.black
+        self.titleLabel.font = UIFont.init(name: "Chalkboard SE", size: 25)
+        
+        self.view.addSubview(self.titleLabel)
+        
+        
+        
+        //서치 바 UI
+        self.searchBar.frame = CGRect(x: 0, y: 105, width: self.view.frame.width, height: 45)
+        self.searchBar.placeholder = "매장 검색"
+        
+        self.view.addSubview(self.searchBar)
+        
+        //테이블 뷰 UI
+        self.tableview.frame = CGRect(x: 0, y: 150, width: self.view.frame.width, height: 654)
+        self.tableview.backgroundColor = UIColor.systemGray5
+        
+        self.view.addSubview(tableview)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
