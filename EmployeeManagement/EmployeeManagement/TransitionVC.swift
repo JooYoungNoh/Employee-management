256a4e6ab4e1714d455686914c83320a2c345874
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-16 20:12:55 +0900

    add : 컨트롤러 파일 추가 및 연결

---
+++ EmployeeManagement/EmployeeManagement/TransitionVC.swift
@@ -0,0 +1,29 @@
+//
+//  TransitionVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/05/16.
+//
+
+import UIKit
+
+class TransitionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
