3dce220534dd4c81162f8dbe7e12b649d11ece3b
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-05-19 22:35:02 +0900

    add: 매장 정보 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/ShopInformationVC.swift
@@ -0,0 +1,19 @@
+//
+//  ShopInformationVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/05/19.
+//
+
+import UIKit
+
+class ShopInformationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
