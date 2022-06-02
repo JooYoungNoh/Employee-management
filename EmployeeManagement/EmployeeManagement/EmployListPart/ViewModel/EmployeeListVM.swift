f59f748cea448a984fb24d216e6f19b655aebea7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-06-03 00:54:07 +0900

    add: ViewModel 파일 생성

---
+++ EmployeeManagement/EmployeeManagement/EmployListPart/ViewModel/EmployeeListVM.swift
@@ -0,0 +1,15 @@
+//
+//  EmployeeListVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/06/03.
+//
+
+import UIKit
+import FirebaseFirestore
+
+class EmployeeListVM {
+    
+    let db = Firestore.firestore()
+    
+}
