fd7596bd88fd4f41b10494c2abaf420e6b349b79
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-06-03 01:08:19 +0900

    add: 내비게이션 바 부분 UI 구성

--- EmployeeManagement/EmployeeManagement/EmployListPart/ViewController/EmployeeListVC.swift
+++ EmployeeManagement/EmployeeManagement/EmployListPart/ViewController/EmployeeListVC.swift
@@ -6,17 +6,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class EmployeeListVC: UIViewController {
+    
+    let tableView = UITableView()                         //테이블 뷰
+    var searchBarController = UISearchController(searchResultsController: nil)  //서치 바
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        uiCreate()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    //액션 메소드
+    @objc func goSetting(_ sender: UIBarButtonItem){
+        
+    }
+    
+    @objc func addFriend(_ sender: UIBarButtonItem){
+        
+    }
+    
+    func uiCreate(){
+        //내비게이션
+        self.navigationItem.title = "Employees"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "CookieRun", size: 30)!]
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "CookieRun", size: 20)!]
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        self.navigationItem.searchController = self.searchBarController
+        
+        //내비게이션 바 버튼
+        let settingButton = UIBarButtonItem(image: UIImage(systemName: "gearshape"), style: .plain, target: self, action: #selector(goSetting(_:)))
+        let addButton = UIBarButtonItem.init(barButtonSystemItem: .add, target: self, action: #selector(addFriend(_:)))
+        
+        self.navigationItem.rightBarButtonItems = [settingButton, addButton]
+    
+        addButton.tintColor = UIColor.black
+        settingButton.tintColor = UIColor.black
+        
     }
 
 }
