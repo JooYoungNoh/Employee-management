c011cbfeff5ba5b964b6bbc7ad32e98c7a583c3b
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-06-03 01:49:43 +0900

    add: 테이블 뷰 셀 UI 구성

---
+++ EmployeeManagement/EmployeeManagement/EmployListPart/ViewController/EmployeeListCell.swift
@@ -0,0 +1,81 @@
+//
+//  EmployeeListCell.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/06/03.
+//
+
+import UIKit
+import SnapKit
+
+class EmployeeListCell: UITableViewCell {
+    
+    static let identifier = "EmployeeListCell"
+    
+    let userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = imageView.frame.width / 2 - 10
+        
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.font = UIFont.init(name: "CookieRun", size: 16)
+        nameLabel.textColor = UIColor.black
+        
+        return nameLabel
+    }()
+
+    let commentLabel: UILabel = {
+        let comment = UILabel()
+        comment.font = UIFont.init(name: "CookieRun", size: 13)
+        comment.textColor = UIColor.systemGray
+        
+        return comment
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(userImageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(commentLabel)
+        
+        userImageView.snp.makeConstraints { make in
+            make.leading.equalTo(20)
+            make.top.equalTo(20)
+            make.width.equalTo(50)
+            make.height.equalTo(50)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.userImageView.snp.trailing).offset(10)
+            make.top.equalTo(5)
+            make.width.equalTo(100)
+            make.height.equalTo(30)
+        }
+        
+        commentLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.nameLabel.snp.leading)
+            make.top.equalTo(self.nameLabel.snp.bottom).offset(5)
+            make.width.equalTo(200)
+            make.height.equalTo(30)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
