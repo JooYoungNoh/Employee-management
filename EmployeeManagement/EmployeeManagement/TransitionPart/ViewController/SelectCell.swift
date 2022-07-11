cd2a679912374c6834aa1b073091787495859a0f
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-07-11 23:01:54 +0900

    add: 테이블 뷰 셀 파일 생성

---
+++ EmployeeManagement/EmployeeManagement/TransitionPart/ViewController/SelectCell.swift
@@ -0,0 +1,23 @@
+//
+//  SelectCell.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/07/11.
+//
+
+import UIKit
+
+class SelectCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
