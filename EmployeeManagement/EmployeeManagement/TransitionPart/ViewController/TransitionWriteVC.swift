04652545b171b58110aa2ca0c6704cbe84b4f39b
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-07-13 22:34:12 +0900

    add : 레시피 작성 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/TransitionPart/ViewController/TransitionWriteVC.swift
@@ -0,0 +1,29 @@
+//
+//  TransitionWriteVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/07/13.
+//
+
+import UIKit
+
+class TransitionWriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
