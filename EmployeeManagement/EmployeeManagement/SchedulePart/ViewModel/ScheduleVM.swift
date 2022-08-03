38075872032fe6cc63bd402a0d60b90d1a1cdd62
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-03 23:41:37 +0900

    add: 스케줄 뷰모델 생성

---
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewModel/ScheduleVM.swift
@@ -0,0 +1,15 @@
+//
+//  ScheduleVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/03.
+//
+
+import FirebaseFirestore
+import FirebaseStorage
+import UIKit
+
+
+class ScheduleVM {
+    
+}
