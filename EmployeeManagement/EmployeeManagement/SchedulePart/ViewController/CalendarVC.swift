0000e7fa59996b296a7796f334d329f669834b88
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-04 06:02:06 +0900

    add: 회사 스케줄 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewController/CalendarVC.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/04.
+//
+
+import UIKit
+
+class CalendarVC: UIViewController {
+    
+    var companyNameOnTable: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("회사 이름 : \(self.companyNameOnTable)")
+    }
+
+
+}
---
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewModel/CalendarVM.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/04.
+//
+
+import FirebaseFirestore
+import FirebaseStorage
+import UIKit
+
+class CalendarVM {
+    
+}
