c30d435612d317c60d44aad87dd0f1bd8287684b
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-09 04:41:21 +0900

    add: 임시 UI 생성

--- EmployeeManagement/EmployeeManagement/SchedulePart/ViewController/TimetableVC.swift
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewController/TimetableVC.swift
@@ -6,24 +6,69 @@
 //
 
 import UIKit
+import SnapKit
 
 class TimetableVC: UIViewController {
 
+    var viewModel = TimetableVM()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "CookieRun", size: 17)
+        label.text = "이름:"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "CookieRun", size: 17)
+        label.text = "시간:"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    //MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.uiCreate()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: 액션 메소드
+    @objc func addTimetable(_ sender: UIBarButtonItem){
+        
     }
-    */
+    
+    //MARK: 화면 메소드
+    func uiCreate(){
+        //내비게이션
+        self.navigationItem.title = "Schedule"  //이따 일월 표시
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont(name: "CookieRun", size: 20)!]
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+        
+        //내비게이션 바 버튼
+        let addButton = UIBarButtonItem.init(barButtonSystemItem: .add, target: self, action: #selector(addTimetable(_:)))
+        
+        self.navigationItem.rightBarButtonItem = addButton
+        addButton.tintColor = UIColor.black
+        
+        //개인 시간 정보
+        self.view.addSubview(self.nameLabel)
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading).offset(45)
+            make.width.equalTo(130)
+            make.height.equalTo(30)
+        }
+        
+        self.view.addSubview(self.timeLabel)
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.equalTo(self.nameLabel.snp.trailing).offset(10)
+            make.width.equalTo(140)
+            make.height.equalTo(30)
+        }
+        
+    }
 
 }
