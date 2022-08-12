3beb693bca26555088ca4920a27fb3fb55f2d4b3
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-13 05:27:28 +0900

    add: 시간표 모델 생성

--- EmployeeManagement/EmployeeManagement/SchedulePart/Model/TimetableModel.swift
+++ EmployeeManagement/EmployeeManagement/SchedulePart/Model/TimetableModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct TimetableModel{
+    
+    var name: String
+    var phone: String
+    var startTime: String
+    var endTime: String
+    var nextday: String
+    var allTime: String
+    var work: String
+    var userCheck: String
+    var date: String
+    
+    init(name: String, phone: String, startTime: String, endTime: String, nextday: String, allTime: String, work: String, userCheck: String, date: String){
+        self.name = name
+        self.phone = phone
+        self.startTime = startTime
+        self.endTime = endTime
+        self.nextday = nextday
+        self.allTime = allTime
+        self.work = work
+        self.userCheck = userCheck
+        self.date = date
+    }
+}
