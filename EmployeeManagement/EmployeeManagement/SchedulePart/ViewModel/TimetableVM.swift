f35de5cf73865dc1a5dcbf5b6bb56781a3bf3974
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-13 05:20:22 +0900

    add: 화면이동 메소드 뷰모델로 이동

--- EmployeeManagement/EmployeeManagement/SchedulePart/ViewModel/TimetableVM.swift
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewModel/TimetableVM.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import UIKit
+import FirebaseFirestore
 
 class TimetableVM {
     
+    let db = Firestore.firestore()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    
+    //MARK: 액션 메소드
+    func addTimetable(uv: UIViewController, companyOnTable: String, dateOnTable: String){
+        if self.appDelegate.jobInfo == "2" {
+            let alert = UIAlertController(title: nil, message: "직원 이상의 직책만 사용가능합니다", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            uv.present(alert, animated: true)
+        } else {
+            guard let nv = uv.storyboard?.instantiateViewController(withIdentifier: "TimetableCreateVC") as? TimetableCreateVC else { return }
+            nv.modalPresentationStyle = .fullScreen
+            nv.companyOnTable = companyOnTable
+            nv.dateOnTable = dateOnTable
+            
+            uv.present(nv, animated: true)
+        }
+    }
 }
