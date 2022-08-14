cefca2485aae3159c41732fdc2aead6a92835e39
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-15 01:37:03 +0900

    add: 시간표 정보 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewController/TimetableInfoVC.swift
@@ -0,0 +1,21 @@
+//
+//  TimetableInfoVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/15.
+//
+
+import UIKit
+
+class TimetableInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewModel/TimetableInfoVM.swift
@@ -0,0 +1,8 @@
+//
+//  TimetableInfoVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/15.
+//
+
+import Foundation
