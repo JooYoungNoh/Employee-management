5d88995d411412f0988aaaf62202c655f940a7d7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-17 22:10:44 +0900

    fix: 셀 초기화
    셀 재사용으로 인해 사진이 뒤죽박죽 해지는 현상 해결

--- EmployeeManagement/EmployeeManagement/SchedulePart/ViewController/ScheduleCell.swift
+++ EmployeeManagement/EmployeeManagement/SchedulePart/ViewController/ScheduleCell.swift
@@ -47,6 +47,11 @@ class ScheduleCell: UICollectionViewCell {
         
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- EmployeeManagement/EmployeeManagement/TransitionPart/ViewController/TransitionCell.swift
+++ EmployeeManagement/EmployeeManagement/TransitionPart/ViewController/TransitionCell.swift
@@ -48,6 +48,10 @@ class TransitionCell: UICollectionViewCell {
         
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
