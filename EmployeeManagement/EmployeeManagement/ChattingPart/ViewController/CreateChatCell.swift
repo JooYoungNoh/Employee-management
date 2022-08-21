80ef3e9b88384395385f29c402ca229973d82784
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-21 23:05:58 +0900

    add: 채팅방 생성 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewController/CreateChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  CreateChatCell.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/21.
+//
+
+import UIKit
+
+class CreateChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewModel/CreateChatVM.swift
@@ -0,0 +1,8 @@
+//
+//  CreateChatVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/21.
+//
+
+import Foundation
