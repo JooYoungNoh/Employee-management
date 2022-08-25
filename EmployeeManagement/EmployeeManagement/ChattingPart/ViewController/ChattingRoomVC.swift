5f63d35aaebcccdf93af899c475dd88e85bdea5d
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-08-26 00:34:23 +0900

    add: 채팅방 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewController/ChattingRoomVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChattingRoomVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/26.
+//
+
+import UIKit
+
+class ChattingRoomVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewModel/ChattingRoomVM.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingRoomVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/08/26.
+//
+
+import Foundation
+
+class ChattingRoomVM {
+    
+}
