a3dbb13f2cd600f97397267be028cc89dc850690
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-09-19 06:46:57 +0900

    add: 모든 사진 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewController/ChattingImageListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChattingImageListVC.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/09/19.
+//
+
+import UIKit
+
+class ChattingImageListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewModel/ChattingImageListVM.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingImageListVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/09/19.
+//
+
+import Foundation
+
+class ChattingImageListVM {
+    
+}
