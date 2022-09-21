1d0348804cfd9e4cd8c7642dbd1772ab4498ea5d
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-09-21 17:17:51 +0900

    add: 채팅창 초대 화면 생성 및 연결

---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewController/ChattingInviteCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingInviteCell.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/09/21.
+//
+
+import UIKit
+
+class ChattingInviteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewModel/ChattingInviteVM.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingInviteVM.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/09/21.
+//
+
+import Foundation
+
+class ChattingInviteVM {
+    
+}
