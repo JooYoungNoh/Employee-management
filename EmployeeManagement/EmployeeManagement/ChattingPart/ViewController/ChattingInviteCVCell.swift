0a7b8996cc97df68beb258006738106f8a67bd62
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-09-21 17:18:41 +0900

    add: 컬랙션 뷰 셀 추가

---
+++ EmployeeManagement/EmployeeManagement/ChattingPart/ViewController/ChattingInviteCVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingInviteCVCell.swift
+//  EmployeeManagement
+//
+//  Created by 노주영 on 2022/09/21.
+//
+
+import UIKit
+
+class ChattingInviteCVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
